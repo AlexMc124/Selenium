62c9f5dbae00e23e797e626699f4d94be8709884
alexm <alex.mcintosh124@gmail.com>
2020-10-04 19:16:54 +0100

    Finished part 3 of the tutorial

--- Selenium pt1&2.py
+++ Selenium pt1&2.py
@@ -20,7 +20,11 @@ try:
     main = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "main"))
     )
-except:
+    articles = main.find_elements_by_tag_name("article")
+    for article in articles:
+        header = article.find_element_by_class_name("entry-title")
+        print(header.text)
+finally:
     driver.quit()
-
+#end of part 2
 print(main.text)
